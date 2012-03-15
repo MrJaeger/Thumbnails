27bb1cdeeb3d904c93322f74662c5107f6d69566
Andrew Jaeger <mrjaeger00@gmail.com>
2012-03-15 03:00:01 -0400

    this should work

--- upload.php
+++ upload.php
@@ -1,5 +1,14 @@
 <?php
 
-echo print_r($_FILES['image_file']);
+$target_path = "assets/full_size/";
+
+$target_path .= $_FILES['image_file']['name'];
+
+if(move_uploaded_file($_FILES['image_file']['tmp_name'], $target_path)) {
+    echo "The file ".  basename( $_FILES['image_file']['name']). 
+    " has been uploaded";
+} else{
+    echo "There was an error uploading the file, please try again!";
+}
 
 ?>
