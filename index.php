2e5875b2013a7aab2b916b06321ac2f8b8420e36
Andrew Jaeger <mrjaeger00@gmail.com>
2012-03-15 01:20:56 -0400

    Starting to build up index.php file

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <html>
 	<head>
 		<title>Thumbnails Site</title>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<script src="./javascript/thumbnails.js"></script>
 	</head>
 	<body>
 		<h1>Thumbnails</h1>
 		<div id="image_list"></div>
+		<form action="upload.php" method="post">
+			<label>Picture to Upload:</label>
+			<input name="file" type="file" />
+			<input type="submit" value="Upload" />
+		</form>
 	</body>
 </html>
