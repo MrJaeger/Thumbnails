fe1a1f4436864f320f22045ed7d4bd6f550fd77d
Andrew Jaeger <mrjaeger00@gmail.com>
2012-03-15 01:55:54 -0400

    trying to get AJAX form uploading working

--- javascript/thumbnails.js
+++ javascript/thumbnails.js
@@ -1,8 +1,6 @@
 $(function () {
 	$('#upload_form').ajaxForm();
 	$('#upload_form').submit(function () {
-		var queryString = $('#myFormId').formSerialize(); 
-		console.log(queryString);
 		//$(this).ajaxSubmit();
 		return false;
 	});
