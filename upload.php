c92fdd413e201e7285f7a8bb3b8969f1ef5fad86
Andrew Jaeger <mrjaeger00@gmail.com>
2012-03-15 02:42:50 -0400

    forgot to change some defaults

--- upload.php
+++ upload.php
@@ -4,8 +4,8 @@ $target_path = "assets/full_size/";
 
 $target_path.= $FILES['image_file']['name'];
 
-if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
-    echo "The file ".  basename( $_FILES['uploadedfile']['name']). 
+if(move_uploaded_file($_FILES['image_file']['tmp_name'], $target_path)) {
+    echo "The file ".  basename( $_FILES['image_file']['name']). 
     " has been uploaded";
 } else{
     echo "There was an error uploading the file, please try again!";
