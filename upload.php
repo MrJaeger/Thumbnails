4ac101df95b21a4c26da307ed7406c452b88d562
Andrew Jaeger <mrjaeger00@gmail.com>
2012-03-15 02:59:08 -0400

    test

--- upload.php
+++ upload.php
@@ -1,18 +1,5 @@
 <?php
 
-echo $FILES['image_file'];
-
-$target_path = "assets/full_size/";
-
-$target_path .= $FILES['image_file']['name'];
-
-exit();
-
-if(move_uploaded_file($_FILES['image_file']['tmp_name'], $target_path)) {
-    echo "The file ".  basename( $_FILES['image_file']['name']). 
-    " has been uploaded";
-} else{
-    echo "There was an error uploading the file, please try again!";
-}
+echo print_r($_FILES['image_file']);
 
 ?>
