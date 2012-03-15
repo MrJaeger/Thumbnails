645dbb705ca842ada7161dd7ad54ad43df5536cd
Andrew Jaeger <mrjaeger00@gmail.com>
2012-03-15 02:03:55 -0400

    trying to get AJAX form uploading working

--- javascript/thumbnails.js
+++ javascript/thumbnails.js
@@ -1,7 +1,12 @@
 $(function () {
-	$('#upload_form').ajaxForm();
-	$('#upload_form').submit(function () {
-		//$(this).ajaxSubmit();
-		return false;
-	});
+	var options = {
+		beforeSubmit: validate,
+		success: output_update
+	};
+	$('#upload_form').ajaxForm(options);
 });
+
+function validate(data, jqForm, options) {
+	var queryString = $('#upload_form').formSerialize(); 
+	return false;
+}
