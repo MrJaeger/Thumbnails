392d2c9ef79a839c98df5b96ce2c6dbdaa692db6
Andrew Jaeger <mrjaeger00@gmail.com>
2012-03-15 16:28:40 -0400

    testing

--- upload.php
+++ upload.php
@@ -6,12 +6,18 @@ $MAX_WIDTH = 900;
 $file_name = $_FILES['image_file']['name'];
 $img = $_FILES['image_file']['tmp_name'];
 
+$results = array();
+array_push($results, getimagesize($img));
+
 $upload = new SimpleImage();
 $upload->load($img);
 
 if($upload->getWidth() > $MAX_WIDTH) {
 	$upload->resizeToWidth($MAX_WIDTH);
 }
+array_push($results, getimagesize($img));
+
+echo print_r($results);
 
 $target_path = "assets/full_size/".$file_name;
 
