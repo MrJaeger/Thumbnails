24b94c3c6df4a3f1a35b0edcb2d18b9813952382
Andrew Jaeger <mrjaeger00@gmail.com>
2012-03-16 03:51:46 -0400

    status for upload

--- javascript/thumbnails.js
+++ javascript/thumbnails.js
@@ -38,15 +38,15 @@ $(function () {
 	}
 
 	function output_update(data, sText, xhr, $form) {
-		file_name = img_data["name"];
-		main_height = img_data["height"];
-		main_width = img_data["width"];
 		var success = "<p>Picture successfully uploaded!</p>";
 		var img_data = $.parseJSON(data);
 		var main_image = "<img id=\"main\" src=\"assets/full_size/" + img_data["name"] + "\" />";
 		var thumbnail_image = "<img style=\"position: relative;\" src=\"assets/full_size/" + img_data["name"] + "\" />";
-		$('#thumbnail_list').html("");
+		file_name = img_data["name"];
+		main_height = img_data["height"];
+		main_width = img_data["width"];
 		$('#status').append(success)
+		$('#thumbnail_list').html("");
 		$('#uploaded').html("").append(main_image);
 		$('#uploaded #main').imgAreaSelect({aspectRatio: '1:1', onSelectChange: update_thumbnail});
 		$('#thumbnail > img').remove();
