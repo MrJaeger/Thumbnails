58b074026945a48b6a30057f21e35cc91911366c
Andrew Jaeger <mrjaeger00@gmail.com>
2012-03-16 02:17:23 -0400

    testing

--- upload_thumbnail.php
+++ upload_thumbnail.php
@@ -11,13 +11,9 @@ $id = uniqid();
 
 $thumbnail = new SimpleImage();
 $thumbnail->load("assets/full_size/".$name);
-
-echo print_r($thumbnail);
-exit();
-
 $thumbnail->resize_within_image($width, $height, $x, $y);
 $thumbnail->resizeToHeight(156);
 $thumbnail->resizeToWidth(156);
-$thumbnail->save("assets/thumbnails/".$id.$name, $upload->image_type);
+$thumbnail->save("assets/thumbnails/".$id.$name, $thumbnail->image_type);
 
 ?>
