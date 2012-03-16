52fdc3c5b9c1f87cc611d8ef91ce4d4789e3b25f
Andrew Jaeger <mrjaeger00@gmail.com>
2012-03-16 01:59:27 -0400

    testing

--- upload_thumbnail.php
+++ upload_thumbnail.php
@@ -2,7 +2,7 @@
 
 require('./helpers/SimpleImage.php');
 
-list($width, $height, $x, $y, $name) = 
+list($width, $height, $x, $y, $name) = $_POST;
 
 $thumbnail = new SimpleImage();
 $thumbnail->load("assets/full_size/".$name);
