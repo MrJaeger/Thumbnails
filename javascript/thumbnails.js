29248f8afe1484707128e01a4d6d0e0c983ef07c
Andrew Jaeger <mrjaeger00@gmail.com>
2012-03-16 01:48:21 -0400

    scoping

--- javascript/thumbnails.js
+++ javascript/thumbnails.js
@@ -6,9 +6,7 @@ $(function () {
 		success: output_update
 	};
 	$('#upload_form').ajaxForm(options);
-	$('#thumbnail_upload_button').click(function(e) {
-		console.log("CLICKED");
-		console.log(main_height, main_width)
+	$('#thumbnail #thumbnail_upload_button').click(function(e) {
 		if(main_width === undefined) {
 			return false;
 		} else {
@@ -23,42 +21,42 @@ $(function () {
 				console.log(data);
 			});
 		}
-	})
-});
-
-function validate(data, jqForm, options) {
-	var imageRegExp = /((.\.png$)|(.\.jpg$)|(.\.gif$))/;
-	if(data.length != 1 || !data[0]["value"]["fileName"].match(imageRegExp)) {
-		return false;
-	} else {
-		$('[class^=imgareaselect]').remove();
-		return true;
-	}
-}
-
-function output_update(data, sText, xhr, $form) {
-	var imgData = $.parseJSON(data);
-	if(imgData["error"] === undefined) {
-		file_name = imgData["name"];
-		main_height = imgData["height"];
-		main_width = imgData["width"];
-		var main_image = "<img id=\"main\" src=\"assets/full_size/" + imgData["name"] + "\" />";
-		$('#uploaded').html("").append(main_image);
-		$('#uploaded #main').imgAreaSelect({aspectRatio: '1:1', onSelectChange: update_thumbnail});
-		var thumbnail_image = "<img style=\"position: relative;\" src=\"assets/full_size/" + imgData["name"] + "\" />";
-		$('#thumbnail > img').remove();
-		$('#thumbnail').append(thumbnail_image);
-		$('#thumbnail_upload_button').css({'top': '170px', 'left': 110});
-	}
-}
-
-function update_thumbnail(img, selection) {
-	var xScale = 156/selection.width;
-	var yScale = 156/selection.height;
-	$("#thumbnail > img").css({
-		width: Math.round(xScale * main_width) + 'px',
-		height: Math.round(yScale * main_height) + 'px',
-		marginLeft: '-' + Math.round(xScale * selection.x1) + 'px',
-		marginTop: '-' + Math.round(yScale * selection.y1) + 'px'
 	});
-}
+
+	function validate(data, jqForm, options) {
+		var imageRegExp = /((.\.png$)|(.\.jpg$)|(.\.gif$))/;
+		if(data.length != 1 || !data[0]["value"]["fileName"].match(imageRegExp)) {
+			return false;
+		} else {
+			$('[class^=imgareaselect]').remove();
+			return true;
+		}
+	}
+
+	function output_update(data, sText, xhr, $form) {
+		var imgData = $.parseJSON(data);
+		if(imgData["error"] === undefined) {
+			file_name = imgData["name"];
+			main_height = imgData["height"];
+			main_width = imgData["width"];
+			var main_image = "<img id=\"main\" src=\"assets/full_size/" + imgData["name"] + "\" />";
+			$('#uploaded').html("").append(main_image);
+			$('#uploaded #main').imgAreaSelect({aspectRatio: '1:1', onSelectChange: update_thumbnail});
+			var thumbnail_image = "<img style=\"position: relative;\" src=\"assets/full_size/" + imgData["name"] + "\" />";
+			$('#thumbnail > img').remove();
+			$('#thumbnail').append(thumbnail_image);
+			$('#thumbnail_upload_button').css({'top': '170px', 'right': '110px'});
+		}
+	}
+
+	function update_thumbnail(img, selection) {
+		var xScale = 156/selection.width;
+		var yScale = 156/selection.height;
+		$("#thumbnail > img").css({
+			width: Math.round(xScale * main_width) + 'px',
+			height: Math.round(yScale * main_height) + 'px',
+			marginLeft: '-' + Math.round(xScale * selection.x1) + 'px',
+			marginTop: '-' + Math.round(yScale * selection.y1) + 'px'
+		});
+	}
+});
