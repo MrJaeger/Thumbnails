76e0178424489e987ced2da543c35551f9f6be11
Andrew Jaeger <mrjaeger00@gmail.com>
2012-03-16 01:56:33 -0400

    testing

--- upload_thumbnail.php
+++ upload_thumbnail.php
@@ -7,6 +7,6 @@ list($width, $height, $x, $y, $name) = $_POST;
 $thumbnail = new SimpleImage();
 $thumbnail->load("assets/full_size/".$name);
 
-echo print_r(array($width, $height, $x, $y, $name));
+echo $_POST['name'];
 
 ?>
